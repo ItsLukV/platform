b9ade5288e5381566b2db180c3581f358f780fef
Lukas <lukasvranic@gmail.com>
2022-01-09 20:06:42 +0100

    clean up

--- script/background.js
+++ script/background.js
@@ -1,9 +1,8 @@
-//85% lukas
+//85% david
 
 /*
 setBackDrop
 canvasCut
-scroll
 */
 
 function setBackDrop(isSetup) {
@@ -38,21 +37,4 @@ function canvasCut() {
   stroke(0);
   fill(0);
   dashboard();
-}
-
-let scrX = 0; // screen ofset from start scene for x
-let scrY = 0; //same but y
-function scroll() {
-  if (mand.x < 50) {
-    scrX = scrX - windowWidth + 150;
-  } else if (mand.x + 100 > windowWidth - 50) {
-    scrX = scrX + windowWidth - 150;
-  }
-
-  while (mand.y <= windowHeight / 8) {
-    scrY--;
-  }
-  while (mand.y + 100 >= windowHeight - windowHeight / 8) {
-    scrY++;
-  }
 }
